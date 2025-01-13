7e4f2ff13e82e862e8f4454df2ac24e906cb32bf
Jesse Chen <jesse.z.chen@gmail.com>
2025-01-13 17:59:14 -0500

    React to year changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,39 @@
 <script lang="ts">
-    import Line from './Line.svelte';
+    import Bar from './Bar.svelte';
 
-    let year: number = 2024;
+    const initialYear: number = 2015;
+    const lines: Array<Line> = [];
+    let year: number = $state(2024);
+
+    class Line {
+        public name: string;
+        public color: string;
+        public reliability: Map<number, number>;
+
+        public constructor(name: string, color: string, performance: Array<number>) {
+            this.name = name;
+            this.color = color;
+            this.reliability = new Map();
+            performance.forEach((perf, index) => {
+                this.reliability.set(index + initialYear, perf);
+            });
+        }
+    }
+
+    lines.push(new Line("A", "blue", 
+        [66.36,	62.43, 54.93, 52.31, 68.90, 81.63, 73.01, 69.06, 72.08, 76.21]));
+    lines.push(new Line("F", "orange", 
+        [57.34, 58.16, 45.70, 45.22, 66.11, 82.11, 74.20, 71.34, 70.15, 69.59]));
 </script>
 
 <main>
     <header>
         <h1>OTP for {year}</h1>
-        <input class="slider" type="range" id="year" min="2015" max="2024" bind:value={year}/>
+        <input class="slider" type="range" id="year" min={initialYear} max="2024" bind:value={year}/>
     </header>
     <svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
-        <Line name="A" color="blue" x={0} y={0} width={202} height={80} />
-        <Line name="F" color="orange" x={0} y={100} width={302} height={80} />
+        <Bar data={lines[0]} index={0} {year} />
+        <Bar data={lines[1]} index={1} {year} />
     </svg>
 </main>
 
---
+++ src/routes/Bar.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    let { data, index, year } = $props();
+    const width = $derived((data.reliability.get(year) || 0) * 10);
+</script>
+
+<rect id={data.name} x=0 y={index*100} {width} height=80 fill={data.color} />
