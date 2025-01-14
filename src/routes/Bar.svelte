db7a98873f268379171e7767ed9f6a25a6826b22
Jesse Chen <jesse.z.chen@gmail.com>
2025-01-14 14:10:05 -0500

    Make bars fit, use standard colors

--- src/routes/Bar.svelte
+++ src/routes/Bar.svelte
@@ -3,8 +3,8 @@
     const width = $derived((data.reliability.get(year) || 0) * 8);
 </script>
 
-<rect id={data.name} x=0 y={index*100} {width} height=80 fill={data.color} />
-<text x={width+20} y={index*100+60} class="bar-label">{data.name} - {data.reliability.get(year)}%</text>
+<rect id={data.name} x=0 y={index*50} {width} height=40 fill={data.color} />
+<text x={width+20} y={index*50+30} class="bar-label">{data.name} - {data.reliability.get(year)}%</text>
 
 <style>
     .bar-label {
