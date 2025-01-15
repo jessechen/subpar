e55a6c07b7f2a3b7aef993738ec781921b3234be
Jesse Chen <jesse.z.chen@gmail.com>
2025-01-15 16:48:17 -0500

    Move line names into circles on the left

--- src/routes/Bar.svelte
+++ src/routes/Bar.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
     let { data, index, year } = $props();
     const width = $derived((data.reliability.get(year) || 0) * 8);
+    const initialY = index*50;
 </script>
 
-<rect id={data.name} x=0 y={index*50} {width} height=40 fill={data.color} />
-<text x={width+20} y={index*50+30} class="bar-label">{data.name} - {data.reliability.get(year)}%</text>
+<circle cx=20 cy={initialY+20} r=20 fill={data.color} />
+<text x=20 y={initialY+30} text-anchor="middle" fill={data.hasBlackText ? "black" : "white"} class="line-label">{data.name}</text>
+<rect x=50 y={initialY} {width} height=40 fill={data.color} />
+<text x={width+70} y={initialY+30} class="bar-label">{data.reliability.get(year)}%</text>
 
 <style>
+    .line-label {
+        font-size: 32px;
+    }
+
     .bar-label {
         font-size: 32px;
     }
