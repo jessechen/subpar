238a3f8034d332e37b27a68f999b330e8a22a9bf
Jesse Chen <jesse.z.chen@gmail.com>
2025-01-17 19:11:28 -0500

    Split up bars with multiple subway lines into two circles

--- src/routes/Bar.svelte
+++ src/routes/Bar.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
     let { data, index, year } = $props();
-    const width = $derived((data.reliability.get(year) || 0) * 8);
-    const initialY = index*50;
+    const width: number = $derived((data.reliability.get(year) || 0) * 8);
+    const initialY: number = index*50;
+
+    const rightName: string = data.name.includes('/') ? data.name.split('/')[1] : data.name
+    const leftName: string = data.name.includes('/') ? data.name.split('/')[0] : ""
 </script>
 
-<circle cx=20 cy={initialY+20} r=20 fill={data.color} />
-<text x=20 y={initialY+30} text-anchor="middle" fill={data.color == "#FCCC0A" ? "black" : "white"} class="line-label">{data.name}</text>
-<rect x=50 y={initialY} {width} height=40 fill={data.color} />
-<text x={width+70} y={initialY+30} class="bar-label">{data.reliability.get(year)}%</text>
+{#if leftName}
+    <circle cx=20 cy={initialY+20} r=20 fill={data.color} />
+    <text x=20 y={initialY+30} text-anchor="middle" fill={data.color == "#FCCC0A" ? "black" : "white"} class="line-label">{leftName}</text>
+{/if}
+<circle cx=70 cy={initialY+20} r=20 fill={data.color} />
+<text x=70 y={initialY+30} text-anchor="middle" fill={data.color == "#FCCC0A" ? "black" : "white"} class="line-label">{rightName}</text>
+<rect x=100 y={initialY} {width} height=40 fill={data.color} />
+<text x={width+120} y={initialY+30} class="bar-label">{data.reliability.get(year)}%</text>
 
 <style>
     .line-label {
